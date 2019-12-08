d271e4614e1dd1f998eee05f4eb65b1c48d5d1c7
V-Icaro <venicius.icarosr@gmail.com>
2019-12-08 02:09:13 -0300

    criação do service ofertas

---
+++ src/app/ofertas.service.ts
@@ -0,0 +1,9 @@
+export class OfertasService {
+    public getOfertas(): string[] {
+        let ofertas = ['Oferta1', 'Oferta2', 'Oferta3']
+        return ofertas
+    }
+}
+
+//serviço para servir componentes com ofertas
+//com metodo que retorna um array de ofertas
