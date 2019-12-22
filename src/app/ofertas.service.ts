d0499aa41866477e10a6c6a2ba8286f60bae7574
V-Icaro <venicius.icarosr@gmail.com>
2019-12-22 11:24:22 -0300

    Capturando Id da pesquisa no banco

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,8 +1,11 @@
 import { Oferta } from './shared/oferta.model'
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs'
+
 
 import { URL_API } from './app.api'
+import 'rxjs/add/operator/map'
 
 @Injectable()
 export class OfertasService {
@@ -46,6 +49,11 @@ export class OfertasService {
         .then((resposta: any) => {
             return resposta[0].descricao
         })
+    }
+
+    public pesquisaOfertas(termo: string): Observable<Oferta[]> {
+        return this.http.get(`${URL_API}/ofertas?descricao_oferta_like=${termo}`)
+            .map((resposta: any) => resposta)
     }
 }
 
--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -1,19 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.service'
+import { Observable } from 'rxjs'
+import { Oferta } from '../shared/oferta.model'
 
 @Component({
   selector: 'app-topo',
   templateUrl: './topo.component.html',
-  styleUrls: ['./topo.component.css']
+  styleUrls: ['./topo.component.css'],
+  providers: [ OfertasService ]
 })
 export class TopoComponent implements OnInit {
 
-  constructor() { }
+  public ofertas: Observable<Oferta[]>
+
+  constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
   }
 
   public pesquisa(busca: string): void {
-    console.log(busca)
-  }
+    this.ofertas = this.ofertasService.pesquisaOfertas(busca)
+
+    this.ofertas.subscribe(
+      (ofertas: Oferta[]) => console.log(ofertas)
+    )
+   }
 
 }
